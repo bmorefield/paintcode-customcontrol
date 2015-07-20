d0ca56aa207546133a10d997c72107ad6100b574
Bill Morefield <bill@billmorefield.com>
2015-07-20 12:49:18 -0400

    Initial Commit

---
+++ PaintCode Control Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PaintCode Control Demo
+//
+//  Created by Bill Morefield on 7/20/15.
+//  Copyright (c) 2015 Bill Morefield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
